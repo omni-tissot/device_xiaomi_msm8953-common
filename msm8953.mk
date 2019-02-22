38186cb5a9a1921070703b4c7d71a333c9594b0e
Nick-infinity <nikhilg.me@gmail.com>
2019-02-22 14:57:15 +0000

    okay

--- msm8953.mk
+++ msm8953.mk
@@ -264,8 +264,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.qcom
+    power.msm8953
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml

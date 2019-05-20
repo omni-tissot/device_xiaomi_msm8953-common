15d1b1d23ccaf526faa1700ea030e3e9147b5f22
Genkzsz11 <genkzsz11@gmail.com>
2019-05-20 14:41:36 +0000

    Implements new parts
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

---
+++ XiaomiParts/src/com/xiaomi/parts/KeyHandler.java
@@ -0,0 +1,139 @@
+/*
+* Copyright (C) 2017 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.xiaomi.parts;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.content.Context;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.os.UserHandle;
+
+import com.android.internal.os.AlternativeDeviceKeyHandler;
+import com.android.internal.util.ArrayUtils;
+
+public class KeyHandler implements AlternativeDeviceKeyHandler {
+
+    private static final String TAG = KeyHandler.class.getSimpleName();
+    private static final boolean DEBUG = true;
+
+    private static final int KEY_HOME_VIRTUAL = 96;
+    private static final int KEY_BACK = 158;
+    private static final int KEY_RECENTS = 139;
+
+    private static final int[] sDisabledKeys = new int[]{
+        KEY_HOME_VIRTUAL,
+        KEY_BACK,
+        KEY_RECENTS
+    };
+
+
+    protected final Context mContext;
+    private Handler mHandler = new Handler();
+    private SettingsObserver mSettingsObserver;
+    private static boolean mButtonDisabled;
+
+    private class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.OMNI_DEVICE_FEATURE_SETTINGS),
+                    false, this);
+            update();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            update();
+        }
+
+
+        public void update() {
+            if (DEBUG) Log.i(TAG, "update called" );
+            setButtonSetting(mContext);
+        }
+    }
+
+    public KeyHandler(Context context) {
+        if (DEBUG) Log.i(TAG, "KeyHandler called");
+        mContext = context;
+        mSettingsObserver = new SettingsObserver(mHandler);
+        mSettingsObserver.observe();
+    }
+
+    @Override
+    public boolean handleKeyEvent(KeyEvent event) {
+        if (DEBUG) Log.i(TAG, "handleKeyEvent called - scancode=" + event.getScanCode() + " - keyevent=" + event.getAction());
+        return false;
+    }
+
+    @Override
+    public boolean canHandleKeyEvent(KeyEvent event) {
+        Log.i(TAG, "canHandleKeyEvent called - scancode=" + event.getScanCode() + " - keyevent=" + event.getAction());
+        return false;
+    }
+
+    @Override
+    public boolean isDisabledKeyEvent(KeyEvent event) {
+        if (DEBUG) Log.i(TAG, "isDisabledKeyEvent called");
+        if (mButtonDisabled) {
+            if (DEBUG) Log.i(TAG, "Buttons are disabled");
+            if (ArrayUtils.contains(sDisabledKeys, event.getScanCode())) {
+                if (DEBUG) Log.i(TAG, "Key blocked=" + event.getScanCode());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void setButtonSetting(Context context) {
+        if (DEBUG) Log.i(TAG, "SetButtonDisable called" );
+        mButtonDisabled = Settings.System.getIntForUser(
+                context.getContentResolver(), Settings.System.OMNI_DEVICE_FEATURE_SETTINGS, 0,
+                UserHandle.USER_CURRENT) == 1;
+        if (DEBUG) Log.i(TAG, "setButtonDisable=" + mButtonDisabled);
+    }
+
+    @Override
+    public boolean isCameraLaunchEvent(KeyEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean isWakeEvent(KeyEvent event){
+        if (DEBUG) Log.i(TAG, "isWakeEvent called - scancode=" + event.getScanCode() + " - keyevent=" + event.getAction());
+        if (event.getAction() != KeyEvent.ACTION_UP) {
+        }
+        return false;
+    }
+
+    @Override
+    public Intent isActivityLaunchEvent(KeyEvent event) {
+        if (event.getAction() != KeyEvent.ACTION_UP) {
+            return null;
+        }
+        return null;
+    }
+}

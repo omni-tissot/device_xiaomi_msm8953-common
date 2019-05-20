4a41b1cebd523c8a1b065c517bcf97c6b67947c9
Genkzsz11 <genkzsz11@gmail.com>
2019-05-20 14:41:36 +0000

    Implements new parts
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

---
+++ XiaomiParts/src/com/xiaomi/parts/DeviceSettings.java
@@ -0,0 +1,109 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.xiaomi.parts;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.res.Resources;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v14.preference.PreferenceFragment;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceManager;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.TwoStatePreference;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.util.Log;
+
+import com.xiaomi.parts.utils.FileUtils;
+
+public class DeviceSettings extends PreferenceFragment {
+
+    private static final String KEY_CATEGORY_DISPLAY = "display";
+    private static final String KEY_HARDWARE_KEYS_DISABLE = "hardware_keys_disable";
+    public static final String KEY_VIBSTRENGTH = "vib_strength";
+
+    private TwoStatePreference mTapToWakeSwitch;
+    private VibratorStrengthPreference mVibratorStrength;
+    private SwitchPreference mKeysDisablesSwitch;
+
+    private Preference mKcalPref;
+    private Preference mAmbientPref;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.main, rootKey);
+
+        mKcalPref = findPreference("kcal");
+        mKcalPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(getContext(), DisplayCalibration.class);
+                startActivity(intent);
+                return true;
+            }
+        });
+
+	mAmbientPref = findPreference("ambient_display_gestures");
+        mAmbientPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(getContext(), TouchscreenGesturePreferenceActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        });
+
+        SwitchPreference mKeysDisablesSwitch = (SwitchPreference) findPreference(KEY_HARDWARE_KEYS_DISABLE);
+        mKeysDisablesSwitch.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.OMNI_DEVICE_FEATURE_SETTINGS, 0) != 0);
+
+        mKeysDisablesSwitch.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+             @Override
+             public boolean onPreferenceClick(Preference preference) {
+                 if (preference == mKeysDisablesSwitch) {
+                     Settings.System.putInt(getActivity().getContentResolver(),
+                             Settings.System.OMNI_DEVICE_FEATURE_SETTINGS, mKeysDisablesSwitch.isChecked() ? 1 : 0);
+                     return true;
+                 }
+                 return false;
+             }
+        });
+		
+        mVibratorStrength = (VibratorStrengthPreference) findPreference(KEY_VIBSTRENGTH);
+        if (mVibratorStrength != null) {
+            mVibratorStrength.setEnabled(VibratorStrengthPreference.isSupported());
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        return super.onPreferenceTreeClick(preference);
+    }
+}
